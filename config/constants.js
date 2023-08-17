7544d655d6cde5e78f2c3846283daa63928d6de1
Ethan Lee <ethanpl@uci.edu>
2023-08-17 14:02:18 -0700

    Render URL

--- config/constants.js
+++ config/constants.js
@@ -1,5 +1,7 @@
+// For Deployment:  https://me-db.onrender.com/
+// For Local:       http://localhost:3000
 const json = {
-    'CLIENT_URL': "http://localhost:3000",
+    'CLIENT_URL': "https://me-db.onrender.com/",
 };
 
 module.exports = json;
