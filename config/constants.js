655ae8715014aec20e6b7f8808142151b9e11fcd
Ethan Lee <ethanpl@uci.edu>
2023-08-21 11:30:38 -0700

    Constants Callback URL

--- config/constants.js
+++ config/constants.js
@@ -1,7 +1,13 @@
-// For Deployment:  https://me-db.onrender.com      https://me-db.vercel.app.com  
-// For Local:       http://localhost:3000
+// For Local
+// const json = {
+//     'CLIENT_URL': "http://localhost:3000",
+//     'SERVER_CALLBACK_URL': "http://localhost:8082/auth/google/callback"
+// };
+
+// For Deployment
 const json = {
     'CLIENT_URL': "https://me-db.vercel.app.com",
+    'SERVER_CALLBACK_URL': "https://me-db.cyclic.cloud.com/auth/google/callback"
 };
 
 module.exports = json;
