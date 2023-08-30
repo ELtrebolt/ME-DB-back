fe2299c91e1bd82e8a7d2fc6be40e648dfb2dbd5
Ethan Lee <ethanpl@uci.edu>
2023-08-29 21:11:32 -0700

    www.me-db.tech

--- config/constants.js
+++ config/constants.js
@@ -6,7 +6,7 @@
 
 // For Deployment
 const json = {
-    'CLIENT_URL': "https://me-db.tech",
+    'CLIENT_URL': "https://www.me-db.tech",
     'SERVER_CALLBACK_URL': "https://me-db.cyclic.cloud/auth/google/callback"
 };
 
